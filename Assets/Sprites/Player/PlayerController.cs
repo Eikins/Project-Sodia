1273130d0bbeb964293ba3149c70719b4cd18d86
Eikins <eric.kinnser@gmail.com>
2019-02-21 19:23:34 +0100

    Finish basic controller

--- Assets/Sprites/Player/PlayerController.cs
+++ Assets/Sprites/Player/PlayerController.cs
@@ -8,35 +8,52 @@ public class PlayerController : MonoBehaviour
 
     [Header("Physics Properties")]
     public float moveSpeed = 1.0f;
+    public float jumpForce = 5.0f;
 
     private float inputMovement;
     private bool inputAttack;
     private bool inputJump;
     
     private bool facingRight;
+    private bool isGrounded;
 
     private Animator animator;
+    private new Rigidbody2D rigidbody;
+
+    private Transform groundCheck;
     
-    // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         facingRight = true;
         animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        groundCheck = transform.Find("GroundCheck");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         Inputs();
-        if(inputAttack) PerformAttack(); else if(inputJump) PerformJump();
+        
+        isGrounded = Physics2D.Linecast(transform.position, groundCheck.position, LayerMask.GetMask("Ground"));
+        animator.SetBool("OnGround", isGrounded);
 
-        animator.SetFloat("MoveSpeed", Mathf.Abs(inputMovement * moveSpeed));
+        if(inputAttack) PerformAttack(); else if(inputJump && isGrounded) PerformJump();
+
+        SetMoveSpeed(inputMovement * moveSpeed);
 
         if(facingRight && inputMovement < -0.1f) {
             Flip();
         } else if(!facingRight && inputMovement > 0.1f) {
             Flip();
         }
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = new Color(1, 1, 0, 0.75F);
+        Gizmos.DrawLine(transform.position, groundCheck.position);
+    }
+
+    private void SetMoveSpeed(float velocity) {
+        rigidbody.velocity = new Vector2(velocity, rigidbody.velocity.y);
+        animator.SetFloat("MoveSpeed", Mathf.Abs(velocity));
     }
 
     private void Inputs() {
@@ -46,11 +63,12 @@ public class PlayerController : MonoBehaviour
     }
 
     private void PerformAttack() {
+        // Hitbox check
         animator.SetTrigger("Attack");
     }
 
     private void PerformJump() {
-        animator.SetTrigger("Jump");
+        rigidbody.AddForce(Vector2.up * jumpForce);
     }
 
     private void Flip() {
